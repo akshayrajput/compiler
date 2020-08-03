f6a7bdb9b081d454fefb934c5cb7134f6d230f2d
Max Base <MaxBaseCode@Gmail.Com>
2020-08-03 19:42:31 +0430

    Update pretty_print.c

--- pretty_print.c
+++ pretty_print.c
@@ -31,73 +31,73 @@ static void pprint_(AST* ast, int indent)
 {
     switch(ast->kind_)
     {
-        case K_NUM: indentation(indent);
+        case TOKEN_K_NUM: indentation(indent);
                     printf("%d\n",ast->d.val_);
                     break;
                     
-        case BE_LT: print_binexpr(indent, "<", ast);
+        case TOKEN_BE_LT: print_binexpr(indent, "<", ast);
                     break;
                     
-        case BE_LTE: print_binexpr(indent, "<=", ast);
+        case TOKEN_BE_LTE: print_binexpr(indent, "<=", ast);
                     break;
                     
-        case BE_GT: print_binexpr(indent, ">", ast);
+        case TOKEN_BE_GT: print_binexpr(indent, ">", ast);
                     break;
 
-        case BE_GTE: print_binexpr(indent, ">=", ast);
+        case TOKEN_BE_GTE: print_binexpr(indent, ">=", ast);
                     break;
 
-        case BE_EQ: print_binexpr(indent, "==", ast);
+        case TOKEN_BE_EQ: print_binexpr(indent, "==", ast);
                     break;
 
-        case BE_NEQ: print_binexpr(indent, "!=", ast);
+        case TOKEN_BE_NEQ: print_binexpr(indent, "!=", ast);
                     break;
 
-        case BE_ADD: print_binexpr(indent, "+", ast);
+        case TOKEN_BE_ADD: print_binexpr(indent, "+", ast);
                     break;
 
-        case BE_SUB: print_binexpr(indent, "-", ast);
+        case TOKEN_BE_SUB: print_binexpr(indent, "-", ast);
                     break;
 
-        case BE_MUL: print_binexpr(indent, "*", ast);
+        case TOKEN_BE_MUL: print_binexpr(indent, "*", ast);
                     break;
 
-        case BE_DIV: print_binexpr(indent, "/", ast);
+        case TOKEN_BE_DIV: print_binexpr(indent, "/", ast);
                     break;
         
-        case K_CALL: indentation(indent);
+        case TOKEN_K_CALL: indentation(indent);
                      printf("callee: %s\n", ast->d.call.name_);
                      for (struct node* tmp = ast->d.call.args_->head; tmp != NULL; tmp = tmp->next)
                         pprint(tmp->data, indent+2);
                     break;
                     
-        case E_ASSIGN: indentation(indent);
+        case TOKEN_E_ASSIGN: indentation(indent);
                        printf("=\n");
                        pprint(ast->d.assign.to_, indent+2);
                        pprint(ast->d.assign.expr_, indent+2);
                     break;
         
-        case ARRAY_ACCESS: indentation(indent);
+        case TOKEN_ARRAY_ACCESS: indentation(indent);
                            printf("Array name: %s\n",ast->d.access.name_);
                            pprint(ast->d.access.expr_, indent+2);
                     break;
                     
-        case VAR_NAME: indentation(indent);
+        case TOKEN_VAR_NAME: indentation(indent);
                        printf("variable name: %s\n",ast->d.variable.name_);
                     break;
        
-        case RETURN:  indentation(indent);
+        case TOKEN_RETURN:  indentation(indent);
                       printf("Return statement\n");
                       pprint(ast->d.ret.expr_, indent+2);
                     break;
                     
-        case WHILE:   indentation(indent);
+        case TOKEN_WHILE:   indentation(indent);
                       printf("While statement\n");
                       pprint(ast->d.while_loop.expr_, indent+2);
                       pprint(ast->d.while_loop.stmt_, indent+2);
                     break;
                     
-        case IF_THEN:  indentation(indent);
+        case TOKEN_IF_THEN:  indentation(indent);
                        printf("If then statement\n");
                        pprint(ast->d.if_then.expr_, indent+2);
                        pprint(ast->d.if_then.if_stmt_, indent+2);
@@ -109,7 +109,7 @@ static void pprint_(AST* ast, int indent)
                        }
                     break;
     
-        case COMPOUND: indentation(indent);
+        case TOKEN_COMPOUND: indentation(indent);
                        printf("Declarations\n");
                        for (struct node* tmp = ast->d.compound.decls_->head; tmp != NULL; tmp = tmp->next)
                           pprint(tmp->data, indent+2);
@@ -120,14 +120,14 @@ static void pprint_(AST* ast, int indent)
                           pprint(tmp->data, indent+2);
                     break;
                     
-        case PARAM:  indentation(indent);
+        case TOKEN_PARAM:  indentation(indent);
                      printf("%s %s%s\n",
                         ast->d.param.type_spec_ == TYPE_INT ? "int" : "void",
                         ast->d.param.name_,
                         ast->d.param.ref_ == 0 ? "" : " []");
                     break;
                   
-        case FUNCTION: indentation(indent);
+        case TOKEN_FUNCTION: indentation(indent);
                        printf("%s %s\n",
                             ast->d.function.ret_type_ ==  TYPE_INT ? "int" : "void", ast->d.function.name_);
                        indentation(indent+2);
@@ -139,7 +139,7 @@ static void pprint_(AST* ast, int indent)
                        pprint(ast->d.function.body_,indent+2);
                     break;
               
-        case VAR_DECL: indentation(indent);
+        case TOKEN_VAR_DECL: indentation(indent);
                        printf("%s %s %d %s\n",
                         ast->d.var_decl.type_spec_ == TYPE_INT ? "int" : "void",
                         ast->d.var_decl.name_,
